4beb20452e6b9f97a5f480c00751aea5c7dd1480
NateDev <beene.nathan@gmail.com>
2024-02-13 22:10:20 -0600

    Forgot to include venv

---
+++ .venv/Lib/site-packages/ffmpeg_progress_yield/__init__.py
@@ -0,0 +1,5 @@
+from .ffmpeg_progress_yield import FfmpegProgress
+
+__version__ = "0.7.8"
+
+__all__ = ["FfmpegProgress"]
---
+++ .venv/Lib/site-packages/ffmpeg_progress_yield/__main__.py
@@ -0,0 +1,47 @@
+import argparse
+
+from . import __version__ as version
+from .ffmpeg_progress_yield import FfmpegProgress
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser(
+        description=f"ffmpeg-progress-yield v{version}",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    parser.add_argument(
+        "-d",
+        "--duration",
+        type=float,
+        help="Duration of the video in seconds (override).",
+    )
+    parser.add_argument(
+        "-n", "--dry-run", action="store_true", help="Print ffmpeg command and exit."
+    )
+    parser.add_argument(
+        "ffmpeg_command",
+        type=str,
+        nargs=argparse.REMAINDER,
+        help="Any ffmpeg command. Do not quote this argument.",
+    )
+    args = parser.parse_args()
+
+    ff = FfmpegProgress(args.ffmpeg_command, dry_run=args.dry_run)
+
+    try:
+        from tqdm import tqdm
+
+        with tqdm(total=100, position=1, desc="Progress") as pbar:
+            for progress in ff.run_command_with_progress(
+                duration_override=args.duration
+            ):
+                pbar.update(progress - pbar.n)
+    except ImportError:
+        for progress in ff.run_command_with_progress():
+            print(f"{progress}/100")
+
+    print(ff.stderr)
+
+
+if __name__ == "__main__":
+    main()
